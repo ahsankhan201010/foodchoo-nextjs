dd8e1133f7d1d0cb0166a11a75cc49ae4ff56c65
MAK <ahsankhan201010@gmail.com>
2021-11-23 18:11:31 +0500

    logo issue resolved

--- uiComponents/navbar/Navbar.js
+++ uiComponents/navbar/Navbar.js
@@ -45,9 +45,7 @@ const Navbar = () => {
       className="navbar">
       <Slider navbar={navbar} setNavbar={setNavbar} />
       <div style={{ alignItems: "flex-start" }} className="logoArea center">
-        <NavLink href="/" exact className="nav-item nav-link">
           <img className="logo" src={Logo.src} style={{ objectFit: "fill" }} />
-        </NavLink>
       </div>
       <div className="menuItemArea">
         <div className="navSimpleItems">
