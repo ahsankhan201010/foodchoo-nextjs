44a127bd179dd7a2232698419fce8440cb5dec0e
shoaib8236 <shoaib.raza.dev@gmail.com>
2021-11-23 15:24:50 +0500

    merged branch with main

--- pages/test/index.jsx
+++ pages/test/index.jsx
@@ -1,12 +1,11 @@
-import MainSVG from "../../uiComponents/mainSVG/MainSVG"
+import MainSVG from "../../uiComponents/mainSVG/MainSVG";
 
 const index = () => {
-    return (
-        <div style={{marginTop:"200px"}} >
-   
-            <MainSVG/>
-        </div>
-    )
-}
+  return (
+    <div style={{ marginTop: "200px" }}>
+      <MainSVG />
+    </div>
+  );
+};
 
-export default index
+export default index;
