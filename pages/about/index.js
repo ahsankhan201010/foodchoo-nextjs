554cd7be4178f6f8b6252622d760417e206f6f88
shoaib8236 <shoaib.raza.dev@gmail.com>
2021-11-23 10:20:28 +0500

    global stylings add

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,12 +1,34 @@
+import Paragraph from "../../uiComponents/paragraph/Paragraph";
+import foodImg from "../../assets/images/sample/food.png";
+import appStoreIcon from "../../assets/images/icons/app-store.png";
+import playStore from "../../assets/images/icons/playstore.png";
 const About = () => {
   return (
     <div className="i-am-about layout">
       <div className="container">
         <div className="row">
           <div className="col-lg-6">
-            <h1 className="theme-title theme-title-black">Years Ago</h1>
+            <h1 className="theme-title theme-title-max theme-title-black">
+              Years Ago
+            </h1>
+            <Paragraph>
+              the founders of FoodChoo realized that restaurant food delivery
+              had several problems which prevented food delivery from reaching
+              its true potential. FoodChoo was developed to help small
+              restaurants sell more meals, to give customers more options and
+              minimal wait times for delivery and to empower the delivery
+              position as a key component of the modern small restaurant
+              business model. <br /> <br /> FoodChoo is about making really fast
+              connections.
+            </Paragraph>
+            <div>
+              <img src={appStoreIcon.src} alt="" />
+              <img src={playStore.src} alt="" />
+            </div>
           </div>
-          <div className="col-lg-6"></div>
+          <div className="col-lg-6 food-image-wrapper">
+            <img src={foodImg.src} alt="" />
+          </div>
         </div>
       </div>
     </div>
